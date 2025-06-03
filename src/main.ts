1423e37ee398f1fe4083473555bc9668316decc8
Nathan Walker <walkerrunpdx@gmail.com>
2025-06-02 23:03:31 -0700

    setup

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { provideZonelessChangeDetection } from '@angular/core';
 import { withInterceptorsFromDi } from '@angular/common/http';
 import { routes } from './app/app.routes';
 import { App } from './app/app';
+import './setup-globals';
 
 runNativeScriptAngularApp({
   appModuleBootstrap: () => {
